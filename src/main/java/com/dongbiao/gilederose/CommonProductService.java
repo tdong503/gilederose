b01c0a09f117e031c836fe3bba6990f834dbff0f
Test <biao.dong@thoughtworks.com>
2020-08-14 22:04:35 +0800

    implement for expired with valid quality case

--- src/main/java/com/dongbiao/gilederose/CommonProductService.java
+++ src/main/java/com/dongbiao/gilederose/CommonProductService.java
@@ -2,8 +2,13 @@ package com.dongbiao.gilederose;
 
 public class CommonProductService {
     public static Product updateProduct(Product product) {
+        if (product.getSellIn() > 0) {
+            product.updateQuality(product.getQuality() - 1);
+        } else {
+            product.updateQuality(product.getQuality() - 2);
+        }
         product.updateSellIn(product.getSellIn() - 1);
-        product.updateQuality(product.getQuality() - 1);
+
         return product;
     }
 }
