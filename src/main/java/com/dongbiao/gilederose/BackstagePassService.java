e2ed861b8695444501a332c74b1d2dae5e458489
Test <biao.dong@thoughtworks.com>
2020-08-14 22:47:27 +0800

    implement for before ten days and valid quality backstage pass product

--- src/main/java/com/dongbiao/gilederose/BackstagePassService.java
+++ src/main/java/com/dongbiao/gilederose/BackstagePassService.java
@@ -2,6 +2,9 @@ package com.dongbiao.gilederose;
 
 public class BackstagePassService {
     public static Product updateProduct(Product product) {
-        return null;
+        if(product.getSellIn() > 10) {
+            product.updateSellIn(product.getSellIn() - 1);
+        }
+        return product;
     }
 }
